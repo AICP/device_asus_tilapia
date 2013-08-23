ca4f77ecabd49ccdd021e6eb0faf7c714d2e2787
Ying Wang <wangying@google.com>
2013-08-23 12:50:36 -0700

    Switch to aosp_* product name in the lunch menu.
    Bug: 10449612
    Change-Id: Iaf83c6c72295e134e368b5165e1a5a81d7c5f81a

--- self-extractors/asus/staging/device-partial.mk
+++ self-extractors/asus/staging/device-partial.mk
@@ -12,13 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Asus blob(s) necessary for Grouper hardware
-PRODUCT_PACKAGES := \
-    sensors-config \
-    apns-conf \
-    camera.tegra3 \
-    sensors.grouper \
-    libsensors.lightsensor \
-    libxgold-ril \
-    libdrmwvmplugin \
-    libwvm
+add_lunch_combo aosp_tilapia-userdebug
